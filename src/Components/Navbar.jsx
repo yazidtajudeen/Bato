ef96dc07893867dd6533ce6cb5a50e61b2f33fd1
yazid tajudeen <yazidtajudeen71@gmail.com>
2024-10-29 14:18:32 +0000

    my first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+'use lient';
+import React from 'react'
+const Links = [
+    { name: 'Home', url: '/Home' },
+    { name: 'About Us', url: '/About' },
+    { name: 'Contact', url: '/Contact' },
+    { name: 'Services', url: '/Services' },
+]
+
+const Navbar = () => {
+  return (
+      <nav>
+          
+   </nav>
+  )
+}
+
+export default Navbar
